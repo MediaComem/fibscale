386685e456e4bcf07e421d994bfa0c9bcea4735d
Simon Oulevay <simon.oulevay@gmail.com>
2022-01-26 21:46:52 +0100

    Add 1-5 seconds wait for locust users

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,8 @@
-from locust import HttpUser, task
+from locust import HttpUser, between, task
 
 class FibScale(HttpUser):
+    wait_time = between(1, 5)
+
     @task
     def compute_fibonacci_number(self):
         self.client.post("/", data={"fib": "100", "algorithm": "iterative"})
