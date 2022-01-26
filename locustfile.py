66b4aa7dc8cb0031f6b78bc892c9d0ebed3571ef
Simon Oulevay <simon.oulevay@gmail.com>
2022-01-26 21:43:51 +0100

    Remove hardcoded delay from locust file

--- locustfile.py
+++ locustfile.py
@@ -3,4 +3,4 @@ from locust import HttpUser, task
 class FibScale(HttpUser):
     @task
     def compute_fibonacci_number(self):
-        self.client.post("/", data={"fib": "100", "algorithm": "iterative", "delay": "1"})
+        self.client.post("/", data={"fib": "100", "algorithm": "iterative"})
